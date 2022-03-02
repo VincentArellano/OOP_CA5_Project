9cda8b4dafdbb06f29bcddf6047131e0fca30dc7
VincentArellano <vincentvarellano08@gmail.com>
2022-03-02 12:51:02 +0000

    Feature 1 Finished

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello world!
  *
@@ -8,7 +13,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("CA5_Project");
-        System.out.println( "Hello World!" );
+        App app = new App();
+        app.start();
+    }
+
+    public void start(){
+        List<Player> namesList = new ArrayList<>();
+        initialiseArrayList(namesList);
+    }
+
+    private void initialiseArrayList(List list){
+        list.add(new Player("Vincent", 18, 1.9));
+        list.add(new Player("Mary", 21, 2.1));
+        list.add(new Player("John", 23, 2.2));
+        list.add(new Player("Luke", 21, 1.6));
+        list.add(new Player("Christian", 25, 1.7));
+        list.add(new Player("Hannah", 24, 1.68));
+        list.add(new Player("Andrea", 22, 1.9));
+        list.add(new Player("James", 27, 1.9));
+        list.add(new Player("Stephan", 30, 1.9));
+        list.add(new Player("Jack", 19, 1.9));
+    }
+
+    private void initialiseHashMap(HashMap list){
+
     }
 }
---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Player {
+    private String name;
+    private int age;
+    private double height;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public Player(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return age == player.age && Double.compare(player.height, height) == 0 && Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, height);
+    }
+}
